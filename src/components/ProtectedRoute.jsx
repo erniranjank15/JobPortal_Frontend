75a050f743c4ec56959f4cd6361782da33afaf5d
erniranjank15 <kasoteniranjan23@gmail.com>
2026-04-04 19:42:38 +0530

    fix: resolve merge conflicts in Profile, Sidebar, ProtectedRoute, Home

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";  
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
@@ -24,31 +23,4 @@ const ProtectedRoute = ({children , role})=>{
 
 }
 
-=======
-import React from "react";  
-import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
-
-
-
-const ProtectedRoute = ({children , role})=>{
-    const {user} = useSelector((state)=>state.auth);
-
-    //Not logged in
-
-    if(!user){
-        return <Navigate to="/login" replace/>
-    }
-
-    //Role based protection
-
-    if(role && user.role !== role){
-        return <Navigate to="/" replace/>
-    }
-
-    return children;
-
-}
-
->>>>>>> 1f3bf29 (updated)
 export default ProtectedRoute;
